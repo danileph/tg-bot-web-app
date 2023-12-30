7534c0fef1561b0a8ddecb2afa17482d8b971671
Danil Efremov <danileph@bk.ru>
2023-12-30 16:44:25 +0300

    Fix: Editing Bar

--- src/components/editing-bar/ui/index.tsx
+++ src/components/editing-bar/ui/index.tsx
@@ -7,39 +7,38 @@ import { Bold, Code, Italic, Strikethrough, Underline } from "lucide-react";
 interface IEditingBarProps extends React.HTMLAttributes<HTMLElement> {}
 
 export const EditingBar = forwardRef<Editor, IEditingBarProps>(({}, ref) => {
-  if (!ref || !("current" in ref) || !ref.current) return null;
-
-  const editor = ref.current;
+  // if (!ref || !("current" in ref) || !ref.current) return null;
+  const editor = ref && "current" in ref && ref.current ? ref.current : null;
 
   return (
     <div className={styles.base}>
       <EditingButton
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        isActive={editor.isActive("bold")}
+        onClick={() => editor?.chain().focus().toggleBold().run()}
+        isActive={editor?.isActive("bold")}
       >
         <Bold width={14} />
       </EditingButton>
       <EditingButton
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        isActive={editor.isActive("italic")}
+        onClick={() => editor?.chain().focus().toggleItalic().run()}
+        isActive={editor?.isActive("italic")}
       >
         <Italic width={14} />
       </EditingButton>
       <EditingButton
-        onClick={() => editor.chain().focus().toggleUnderline().run()}
-        isActive={editor.isActive("underline")}
+        onClick={() => editor?.chain().focus().toggleUnderline().run()}
+        isActive={editor?.isActive("underline")}
       >
         <Underline width={14} />
       </EditingButton>
       <EditingButton
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        isActive={editor.isActive("strike")}
+        onClick={() => editor?.chain().focus().toggleStrike().run()}
+        isActive={editor?.isActive("strike")}
       >
         <Strikethrough width={14} />
       </EditingButton>
       <EditingButton
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        isActive={editor.isActive("code")}
+        onClick={() => editor?.chain().focus().toggleCode().run()}
+        isActive={editor?.isActive("code")}
       >
         <Code width={14} />
       </EditingButton>
