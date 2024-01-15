d76c6198ff783e13aafd5d77b01bacf13f53a7dc
Danil Efremov <danileph@bk.ru>
2024-01-15 12:23:50 +0300

    Config has been added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sass from "sass";
+import VitePluginHtmlEnv from "vite-plugin-html-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginHtmlEnv(),
+    VitePluginHtmlEnv({
+      compiler: true,
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
