41fc49995795e8ada86b8e6d195a677a92812443
Danil Efremov <danileph@bk.ru>
2024-02-06 01:15:29 +0300

    Finding bugs with HTML line break parsing

--- src/lib/helpers/BrToN.ts
+++ src/lib/helpers/BrToN.ts
@@ -1,6 +1,6 @@
 export function brToN(inputString: string): string {
   // Define the regular expression pattern
-  const pattern = /<br \/>/g;
+  const pattern = /<br\s*\/?>/g;
 
   // Use the replace method to replace "<br />" with "\n"
   const resultString = inputString.replace(pattern, "\n");
